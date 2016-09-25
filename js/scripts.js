cbed6911051c8ae5002edc851c28cddbb212908a
scarletfog <scarletfog@gmail.com>
2016-09-25 17:09:15 +0200

    ciagi

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach';
+var animal = 'Zielone słonie';
+var animal = animal.toUpperCase();
+
+var text = text.replace('Papugi', animal);
+
+console.log(text.substr(0, 44));
